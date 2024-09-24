aae3ba14d806ea5b25d036241c71a94a8db743eb
rogawa <rogawa@ubuntu-dt01.lan>
2024-09-25 00:00:57 +0900

    設定同期用すクリプトを追加

--- sync_config.sh
+++ sync_config.sh
@@ -1,10 +1,21 @@
-confirm_prompt="Fully sync your current vim configuration with the main repository.\nLocal changes are permanently discarded.\nDo you want to continue? yes/no:"
-echo -en $confirm_prompt" "
+# define constants
+CONFIRM_PROMPT="Fully sync your current vim configuration with the main repository.\nLocal changes are permanently discarded.\nDo you want to continue? yes/no:"
+GIT_CMD_FEATCH="git fetch origin main"
+GIT_CMD_RESET="git reset --hard origin/main"
+
+# confirm
+echo -en $CONFIRM_PROMPT" "
 read is_confirm
+
 if [ $is_confirm == "yes" ]; then
-    git fetch origin main
-    git reset --hard origin/main
+    # sync force
+    echo -e "\e[32m-- ${GIT_CMD_FEATCH}\e[0m"
+    $GIT_CMD_FEATCH
+    echo -e "\e[32m-- ${GIT_CMD_RESET}\e[0m"
+    $GIT_CMD_RESET
 else
+    # abort
     echo "aborted."
 fi
+
 exit
