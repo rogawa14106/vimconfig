3ecd58004b3fa832b6530abaa2b541d689ec6346
rogawa14106 <ookawa14106@gmail.com>
2024-10-19 19:40:05 +0900

    chg: nvim-translatorのブランチをdevに修正

--- lua/plugins/nvim-translator.lua
+++ lua/plugins/nvim-translator.lua
@@ -1,5 +1,5 @@
 return {
-    "rogawa14106/nvim-translator",
+    {"rogawa14106/nvim-translator", branch = 'dev'},
     config = function()
         local nvim_translator = require('nvim-translator')
     end,
