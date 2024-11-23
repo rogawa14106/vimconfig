d4c6f5c837ee939d468f13590fc5953ed57443b0
rogawa14106 <ookawa14106@gmail.com>
2024-11-23 18:31:43 +0900

    refactor: vimscriptの生き残りをluaに書き換え。ファイルタイプ検知のautocmdを移動。

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,31 +1,48 @@
-vim.cmd [[
-"{{{ save and load fold state
-function! SaveFold() abort
-    try
-        mkview
-    catch
-    endtry
-endfunction
-
-function! LoadFold() abort
-    try
-        loadview
-        "call HighlightEcho("info", "load fold view")
-    catch
-    endtry
-endfunction
-
-augroup AutoSaveFold
-    autocmd!
-    autocmd BufWinLeave * call SaveFold()
-    autocmd BufReadPost * call LoadFold()
-augroup END
-"}}}
-]]
-
+-- change filetypes
+local augrp_ft = vim.api.nvim_create_augroup("UserFiletype", {})
+-- clang header file{{{
 vim.api.nvim_create_autocmd('BufEnter', {
+    group = augrp_ft,
     pattern = { "*.h" },
     callback = function()
         vim.cmd('set filetype=c')
     end,
 })
+-- }}}
+
+-- terraform satte file{{{
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    group = augrp_ft,
+    pattern = { "*.tfstate", "*.tfstate.backup" },
+    callback = function()
+        vim.bo.filetype = "json"
+    end
+})
+-- }}}
+
+-- folding
+local augrp_fold = vim.api.nvim_create_augroup("KeepFoldState", {})
+-- save and load folding state{{{
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+    group = augrp_fold,
+    callback = function()
+        vim.cmd [[
+            try
+                mkview
+            catch
+            endtry
+        ]]
+    end,
+})
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+    group = augrp_fold,
+    callback = function()
+        vim.cmd [[
+            try
+                loadview
+            catch
+            endtry
+        ]]
+    end,
+})
+-- }}}
