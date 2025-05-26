e5eaf6d22a19d6ad4ac2c9694b23f7f1a5e6663b
rogawa14106 <ookawa14106@gmail.com>
2025-05-26 13:02:02 +0900

    feat: 画像ファイルをOS固有の画像ビュワーで開けるように設定.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -36,6 +36,11 @@ return {
                     window_picker = {
                         enable = false,
                     }
+                },
+                file_popup = {
+                    open_win_config = {
+                        border = "rounded",
+                    },
                 }
             },
             filters = {
@@ -63,6 +68,51 @@ return {
                 vim.keymap.set('n', '<C-[>', api.tree.change_root_to_parent, opts('Up'))
                 -- toggle help
                 vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+                -- open in image viewr
+                vim.keymap.set('n', 'i', function()
+                    -- カーソル下のノード取得
+                    local node = api.tree.get_node_under_cursor()
+                    -- パス取得
+                    local abs_path = node.absolute_path
+
+                    -- 画像ファイルでない場合は終了
+                    -- TODO luaの正規表現でパイプが使えない。いままでどうしてたっけ。
+                    local ext = string.match(abs_path, "%..+$") or ""               -- 対象ファイルの拡張子
+                    local image_ext_list = { "jpeg", "jpg", "png", "svg", "webp", } -- 画像ファイル拡張子の一覧
+                    local is_image_file = false                                     -- 画像ファイルかどうか
+
+                    for _, image_ext in ipairs(image_ext_list) do
+                        if "." .. image_ext == ext then
+                            is_image_file = true
+                            break
+                        end
+                    end
+                    -- 画像ファイルではない場合は、メッセージを出して終了。
+                    if is_image_file == false then
+                        vim.notify(
+                            "'" .. ext .. "' is not a image file. " ..
+                            "if you want to open this file as an image file, add this file extension to image_ext_list.",
+                            vim.log.levels.WARN)
+                        return
+                    end
+
+                    -- osごとに画像ビュワーを変更
+                    local cmd = ""
+                    if vim.fn.has('linux') then
+                        cmd = "eog " .. abs_path
+                    elseif vim.fn.has('win32') or vim.fn.has('win64') then
+                        cmd = ""
+                        -- cmd = 'powershell -Command Invoke-Item " ' .. abs_path .. '"'
+                    end
+                    -- コマンドを設定していないOSは何もしない
+                    if cmd == "" then
+                        vim.notify("the cmd to open image viewer is not configured on this OS.", vim.log.levels.ERROR)
+                        return
+                    end
+
+                    -- 画像ビュワーを非同期で開く
+                    vim.fn.jobstart(cmd)
+                end, opts('Open: Image viewer'))
             end,
         })
 
